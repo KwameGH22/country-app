7fcf225798a7ecca6f496998db2acbb11f51ced2
KwameGH22 <adarkwa.y.k@gmail.com>
2024-02-07 17:41:29 +0000

    last commit

--- src/pages/CountryProfile.jsx
+++ src/pages/CountryProfile.jsx
@@ -7,7 +7,7 @@ import {
   useParams,
 } from "react-router-dom";
 import { useAppContext } from "../useAppContext";
-import { Context } from "../context/Context"; 
+import { Context } from "../context/Context";
 
 //loader function
 // export const loader = async ({ req, params }) => {
@@ -20,13 +20,23 @@ import { Context } from "../context/Context";
 
 const CountryProfile = () => {
   const { countryName } = useParams();
-  const { allCountries } = useContext(Context);
+  // const { allCountries } = useContext(Context);
+  const [countries, setCountries] = React.useState([]);
 
-  const country = allCountries?.find(
+  useEffect(() => {
+    const getCountry = async (name) => {
+      const response = await fetch(`https://restcountries.com/v3.1/all`);
+      const data = await response.json();
+      setCountries(data);
+    };
+    getCountry();
+  }, []);
+
+  const country = countries?.find(
     (country) => country.name.common === countryName
   );
 
-  console.log(country);
+  console.log(countries);
 
   const {
     name,
@@ -39,111 +49,110 @@ const CountryProfile = () => {
     languages,
     flags,
     borders,
-  } = country;
+  } = country || {};
   // console.log(country)
 
   //Get Native name
-  const nativeKeys = Object.keys(name.nativeName);
+  const nativeKeys = (name && Object.keys(name?.nativeName)) || "";
   // const nativeNames = nativeKeys.map(key => (name.nativeName[key].common)).join(", ")
   const nativeName =
-    name.nativeName[nativeKeys[nativeKeys.length - 1]]["common"];
+    name?.nativeName[nativeKeys[nativeKeys.length - 1]]["common"];
 
   //Get currencies
-  const currencyName = Object.keys(currencies)[0]; //This gets the first item in the currencies object
-  const usedCurrencies = currencies[currencyName]; //This gets the object that has the currency name
+  const currencyName = name && Object.keys(currencies)[0]; //This gets the first item in the currencies object
+  const usedCurrencies = currencies && currencies[currencyName]; //This gets the object that has the currency name
 
   //Get languages
-  const spokenLanguages = Object.values(languages)
-    .map((language) => language)
-    .join(", "); //This gets the languages
+  const spokenLanguages =
+    languages &&
+    Object.values(languages)
+      .map((language) => language)
+      .join(", "); //This gets the languages
 
   return (
-    <div className="container grid gap-12 py-12 items-start text-[#111517] bg-[#FFFFFF]  dark:bg-[#2B3844] dark:text-[#FFFFFF] ">
-      <Link to={"/"} className="text-black font-light w-max">
-        <button className="px-4 py-2  bg-white rounded-sm shadow-lg dark:bg-[#2B3844] dark:text-[#FFFFFF] ">
-          ← Back
-        </button>
-      </Link>
-      <div
-        className="grid xl:grid-cols-2 gap-20
-      "
-      >
-        <div className="w-full max-w-full">
-          <img src={flags?.svg} alt={flags.alt} className="max-w-full" />
-        </div>
-        <div className="grid gap-8">
-          <h3 className="font-semibold">{name.common}</h3>
-          <div className="flex flex-col items-start md:flex-row  gap-20 ">
-            <div className="">
-              <div className="grid gap-2">
-                <div className="flex gap-2 items-center">
-                  <h6>Native Name:</h6>
-                  <span>{nativeName}</span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <h6>Population:</h6>
-                  <span>{population.toLocaleString()}</span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <h6>Region:</h6>
-                  <span>{region}</span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <h6>Sub Region:</h6>
-                  <span>{subregion}</span>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <h6>Capital:</h6>
-                  <span>{capital[0]}</span>
-                </div>
-              </div>
+    <>
+      {countries ? (
+        <div className=" w-[90%] min-width-[90%] m-auto grid gap-12 py-12 items-start text-[#111517] bg-[#FFFFFF]  dark:bg-[#2B3844] dark:text-[#FFFFFF] ">
+          <Link to={"/"} className="text-black font-light w-max">
+            <button className="px-4 py-2  bg-white rounded-sm shadow-lg dark:bg-[#2B3844] dark:text-[#FFFFFF] ">
+              ← Back
+            </button>
+          </Link>
+          <div className="grid xl:grid-cols-2 gap-20">
+            <div className="w-full max-w-full">
+              <img src={flags?.svg} alt={flags?.alt} className="max-w-full" />
             </div>
-            <div className="grid gap-2">
-              <div className="flex gap-2 items-center">
-                <h6>Top Level Domain:</h6>
-                <span>{tld[0]}</span>
+            <div className="grid gap-8">
+              <h3 className="font-semibold">{name?.common}</h3>
+              <div className="flex flex-col items-start md:flex-row  gap-20 ">
+                <div className="">
+                  <div className="grid gap-2">
+                    <div className="flex gap-2 items-center">
+                      <h6>Native Name:</h6>
+                      <span>{nativeName}</span>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <h6>Population:</h6>
+                      <span>{population?.toLocaleString()}</span>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <h6>Region:</h6>
+                      <span>{region}</span>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <h6>Sub Region:</h6>
+                      <span>{subregion}</span>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                      <h6>Capital:</h6>
+                      <span>{capital && capital[0]}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex gap-2 items-center">
+                    <h6>Top Level Domain:</h6>
+                    <span>{tld && tld[0]}</span>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <h6>Currencies:</h6>
+                    <span>
+                      {usedCurrencies?.name} (
+                      {usedCurrencies?.symbol && usedCurrencies.symbol}){" "}
+                    </span>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <h6>Languages:</h6>
+                    <span>{spokenLanguages}</span>
+                  </div>
+                </div>
               </div>
               <div className="flex gap-2 items-center">
-                <h6>Currencies:</h6>
-                <span>
-                  {usedCurrencies.name} (
-                  {usedCurrencies.symbol && usedCurrencies.symbol}){" "}
-                </span>
+                <h6>Border Countries:</h6>
+                <div className={"flex gap-2 flex-wrap items-start"}>
+                  {borders?.map((border, index) => (
+                    <Link
+                      key={index}
+                      to={`/${
+                        countries?.find((country) => country.cca3 === border)
+                          .name["common"]
+                      }`}
+                      className="text-black"
+                    >
+                      <span className="py-1 px-2 shadow-lg  bg-white rounded-sm">
+                        {border}
+                      </span>
+                    </Link>
+                  ))}
+                </div>
               </div>
-              <div className="flex gap-2 items-center">
-                <h6>Languages:</h6>
-                <span>{spokenLanguages}</span>
-              </div>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center">
-            <h6>Border Countries:</h6>
-            <div className={'flex gap-2 flex-wrap items-start'}>
-              {/* {borders?.map((border, index) => (
-                <Link
-                  key={index}
-                  to={`/${
-                    countries.find((country) => country.cca3 === border).name[
-                      "common"
-                    ]
-                  }`}
-                  className="text-black"
-                >
-                  <span
-                    className={`py-1 px-2 shadow-lg  bg-white rounded-sm ${
-                      theme === "dark" && "dark-component"
-                    }`}
-                  >
-                    {border}
-                  </span>
-                </Link>
-              ))} */}
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
   );
-    // return <h1>country</h1>
 };
 export default CountryProfile;
